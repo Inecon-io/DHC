15d20f1d4c83930040e8864f29a5db82aa0039d8
malyca <60945411+malyca@users.noreply.github.com>
2020-12-01 00:03:08 +0100

    Commits for beginning

---
+++ DHC/lib/login.dart
@@ -0,0 +1,256 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+import 'package:DHC/globalVariables.dart';
+
+import 'home.dart';
+
+class Login extends StatefulWidget {
+  @override
+  _LoginViewState createState() => _LoginViewState();
+}
+
+String UserMail = "";
+
+class _LoginViewState extends State<Login> {
+  bool _showPassword = false;
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final mq = MediaQuery.of(context);
+    future:
+    Firebase.initializeApp();
+    void showAlertDialog(BuildContext context) {}
+
+    final imageLogo =
+        Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Image.asset(
+        'asset/Logo_2020_04_Sort.png',
+        fit: BoxFit.contain,
+        height: 100,
+      ),
+    ]);
+
+    final emailField = TextFormField(
+      controller: _emailController,
+      keyboardType: TextInputType.emailAddress,
+      style: TextStyle(
+        color: Colors.black,
+      ),
+      cursorColor: Colors.black,
+      decoration: InputDecoration(
+        suffixIcon: Icon(
+          Icons.email,
+          color: Colors.grey,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide(color: Color(0xfffb8900), width: 2.0),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(30.0)),
+          borderSide: BorderSide(color: Colors.black, width: 1.0),
+        ),
+        labelText: 'mail@mail.dk',
+        labelStyle: TextStyle(fontFamily: "NunitoSans", color: Colors.grey),
+        hintText: 'email',
+        hintStyle: TextStyle(fontFamily: "NunitoSans", color: Colors.black),
+      ),
+    );
+
+    final passwordField = Column(
+      children: <Widget>[
+        TextFormField(
+          obscureText: true,
+          controller: _passwordController,
+          style: TextStyle(
+            color: Colors.black,
+          ),
+          cursorColor: Colors.black,
+          decoration: InputDecoration(
+            suffixIcon: IconButton(
+              icon: Icon(Icons.visibility),
+              color: Colors.grey,
+              onPressed: () {
+                setState(() => this._showPassword = !this._showPassword);
+              },
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: Color(0xfffb8900), width: 2.0),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.all(Radius.circular(30.0)),
+              borderSide: BorderSide(color: Colors.black, width: 1.0),
+            ),
+            labelText: 'password',
+            labelStyle: TextStyle(fontFamily: "NunitoSans", color: Colors.grey),
+            hintText: 'password',
+            hintStyle: TextStyle(fontFamily: "NunitoSans", color: Colors.black),
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(2.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: <Widget>[
+            MaterialButton(
+                child: Text(
+                  "Forgot Password",
+                  style: Theme.of(context)
+                      .textTheme
+                      .caption
+                      .copyWith(color: Colors.white),
+                ),
+                onPressed: () {
+                  showAlertDialog(context);
+                }),
+          ],
+        ),
+      ],
+    );
+
+    final fields = Padding(
+      padding: EdgeInsets.only(top: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: <Widget>[
+          imageLogo,
+          SizedBox(height: 50),
+          emailField,
+          SizedBox(height: 20),
+          passwordField,
+        ],
+      ),
+    );
+
+    final loginButton = Material(
+      elevation: 5.0,
+      borderRadius: BorderRadius.circular(25.0),
+      color: NovotiOrange,
+      child: MaterialButton(
+        minWidth: mq.size.width / 1.2,
+        padding: EdgeInsets.fromLTRB(10.0, 15.0, 10.0, 15.0),
+        child: Text(
+          "Login",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 20.0,
+            color: Colors.white,
+            fontFamily: "NunitoSansBold",
+          ),
+        ),
+        onPressed: () async {
+          try {
+            // ignore: deprecated_member_use
+            final User user =
+                (await FirebaseAuth.instance.signInWithEmailAndPassword(
+              email: _emailController.text,
+              password: _passwordController.text,
+            ))
+                    .user;
+            if (user != null) {
+              SharedPreferences prefs = await SharedPreferences.getInstance();
+              prefs.setString('displayName', user.displayName);
+              prefs.setString('email', user.email);
+              print(user.email);
+              username = user.email;
+              // ignore: deprecated_member_use
+              Firestore.instance
+                  .collection("Users")
+                  .where("email", isEqualTo: user.email)
+                  .get()
+                  .then((querySnapshot) {
+                querySnapshot.docs.forEach((result) {
+                  print(result.data()["Navn"]);
+                  prefs.setString("Name", result.data()["Navn"]);
+                });
+              });
+
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => HomePage(),
+                ),
+              );
+            }
+          } catch (e) {
+            print(e);
+            _emailController.text = "";
+            _passwordController.text = "";
+          }
+        },
+      ),
+    );
+
+    final bottom = Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: <Widget>[
+        loginButton,
+        Padding(
+          padding: EdgeInsets.all(8.0),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              "Not a member?",
+              style: Theme.of(context).textTheme.subtitle1.copyWith(
+                    color: Colors.white,
+                  ),
+            ),
+            MaterialButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomePage(),
+                  ),
+                );
+              },
+              child: Text(
+                "Sign Up",
+                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                      color: Colors.white,
+                      decoration: TextDecoration.underline,
+                    ),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Form(
+        key: _formKey,
+        child: SingleChildScrollView(
+          padding: EdgeInsets.all(36),
+          child: Container(
+            height: mq.size.height,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                fields,
+                Padding(
+                  padding: EdgeInsets.only(bottom: 150),
+                  child: bottom,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
